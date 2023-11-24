461c22a246112ba76b2912c1fb97729f6ffac836
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-11-24 19:12:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Player from './components/player';
+function App() {
+	return (
+		<main>
+			<div id="game-container">
+				<ol id="players">
+					<Player
+						name="Player 1"
+						symbol="X"
+					/>
+					<Player
+						name="Player 2"
+						symbol="O"
+					/>
+				</ol>
+				Game board
+			</div>
+		</main>
+	);
+}
+
+export default App;
---
+++ src/components/player.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+export default function Player({ name, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [playerName, setplayerName] = useState(name);
+	let ediatblePlayerName = <span className="player-name">{playerName}</span>;
+	if (isEditing) {
+		ediatblePlayerName = (
+			<input
+				type="text"
+				required
+				value={playerName}
+				onChange={handleChange}
+			/>
+		);
+	}
+	function handleEditClick() {
+		setIsEditing((editing) => !editing);
+	}
+	function handleChange(event) {
+		setplayerName(event.target.value);
+	}
+	return (
+		<li>
+			<span className="player">
+				{ediatblePlayerName}
+				<span className="player-symbol">{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+		</li>
+	);
+}
